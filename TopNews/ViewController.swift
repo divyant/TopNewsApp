13035e080a98eac6d60c51b86665194142bb8765
Divyant Srivastava <dsrivastava13@sapient.com>
2020-01-19 00:50:40 +0530

    Initial Commit

---
+++ TopNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopNews
+//
+//  Created by Divyant Srivastava on 19/01/20.
+//  Copyright © 2020 Divyant Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
